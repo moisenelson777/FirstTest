019442171a0aec91539797f8779a6fb269eeab3b
moise <moise@192.168.1.93>
2018-11-07 05:49:03 +0100

    Troisième modification

--- src/AutreTest.java
+++ src/AutreTest.java
@@ -4,6 +4,7 @@ public class AutreTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Jesus Christ est mon sauveur ! ");
+		System.out.println("Je suis sure et certains que mes efforts finiront par payer");
 	}
 
 }
