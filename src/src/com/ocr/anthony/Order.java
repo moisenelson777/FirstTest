f73bc49dd852f9f0485adbadd78546f8c9037bcf
moise <moise@192.168.1.93>
2018-11-08 03:39:51 +0100

    Structurer le code avec des TDD

--- src/src/com/ocr/anthony/Order.java
+++ src/src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package src.com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -28,5 +30,17 @@ public class Order {
     	else if(nbMenu < 1 || nbMenu > 3) {
     		System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
     	}
+    }
+    
+    /**
+     * Run asking process for a new menu
+     * */
+    public void runMenu() {
+    	 this.displayAvailableMenu();
+         Scanner sc = new Scanner(System.in);
+         int nb = sc.nextInt();
+         this.displaySelectedMenu(nb);
+         
+         sc.close();
     }
 }
--- src/test/com/ocr/anthony/OrderTest.java
+++ src/test/com/ocr/anthony/OrderTest.java
@@ -7,6 +7,7 @@ import src.com.ocr.anthony.Order;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -53,5 +54,14 @@ public class OrderTest {
     public void Given_NegativeValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
         order.displaySelectedMenu(-6);
         assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+    }
+    @Test
+    public void Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+        System.setIn(new ByteArrayInputStream("1\n".getBytes()));
+        order = new Order();
+        order.runMenu();
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals(output.endsWith("Vous avez choisi comme menu : poulet\n"), true);
+        assertEquals(output.length() > "Vous avez choisi comme menu : poulet\n".length(), true);
     }
 }
